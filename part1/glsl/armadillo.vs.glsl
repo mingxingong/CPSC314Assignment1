c23360d78cfea6dd82b1bf26b03c9ee05e0af823
mingxingong <57270677+mingxingong@users.noreply.github.com>
2022-01-26 21:40:35 -0800

    removed unneccessary lines

--- part1/glsl/armadillo.vs.glsl
+++ part1/glsl/armadillo.vs.glsl
@@ -14,15 +14,8 @@ void main() {
     
     mat4 invModelMatrix = inverse(modelMatrix);
     vec3 mvOrbPosition = vec3(invModelMatrix * vec4(orbPosition, 1.0));
-    // vec4 modelViewPosition4 = invModelMatrix * vec4(position, 1.0);
-    // vec3 modelViewPosition3 = vec3(modelViewPosition4);
     vec3 lightDirection = normalize(mvOrbPosition - position);
-    // vec4 modelViewNormal4 = invModelMatrix * vec4(normal, 1.0);
-    // vec3 modelViewNormal3 = vec3(modelViewNormal4);
-    vec3 wvNormal = vec3(modelMatrix * vec4(normal, 1.0));
-    // vec3 lightDirection = orbPosition - wvPosition;
     vcolor = dot(normal, lightDirection);
-    // vcolor = 0.5; // REPLACE ME
 
     // Q1D:
     // HINT: Compute distance in World coordinate to make the magnitude easier to interpret
